b74768539029f81c4a3ca5643aa165ffa0ad1e89
Bank_Security <m.molte@hotmail.it>
2019-10-02 16:18:49 +0200

    Create Screenshot.ps1

---
+++ Discovery/Screenshot.ps1
@@ -0,0 +1,23 @@
+#############################################################################
+# Capturing a screenshot
+#############################################################################
+$File = "C:\Windows\Temp\Secret.bmp"
+Add-Type -AssemblyName System.Windows.Forms
+Add-type -AssemblyName System.Drawing
+# Gather Screen resolution information
+$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+$Width = $Screen.Width
+$Height = $Screen.Height
+$Left = $Screen.Left
+$Top = $Screen.Top
+# Create bitmap using the top-left and bottom-right bounds
+$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+# Create Graphics object
+$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+# Capture screen
+$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+# Save to file
+$bitmap.Save($File) 
+Write-Output "Screenshot saved to:"
+Write-Output $File
+#############################################################################
