bb3550f8f6ef3f1835d014a382cb215022fa138c
Bank_Security <m.molte@hotmail.it>
2019-10-10 14:19:18 +0200

    Update Host_Recon_WMIC.bat

--- Discovery/Host_Recon_WMIC.bat
+++ Discovery/Host_Recon_WMIC.bat
@@ -1,5 +1,5 @@
-echo on HOST Enumeration:
-echo on --- OS Specifics ---
+echo HOST Enumeration:
+echo ::::OS Specifics::::
 wmic OS get Caption,OSArchitecture,Version
 wmic BIOS get Manufacturer,Name,SMBIOSBIOSVersion,Version
 wmic CPU get Name,NumberOfCores,NumberOfLogicalProcessors
@@ -11,19 +11,19 @@ wmic process get Name, executablepath, processid
 wmic startup get Caption,Command,Location,User
 wmic PRODUCT get Description,InstallDate,InstallLocation,Vendor,Version
 
-echo on :: --- Local User Accounts ---
+echo ::::Local User Accounts::::
 wmic USERACCOUNT Get Domain,Name,Sid
 wmic USERACCOUNT list full
 wmic useraccount get /ALL
 
 
-echo on --- Netowrk Config ---
+echo ::::Netowrk Config::::
 wmic nicconfig where IPEnabled='true' get Caption,DefaultIPGateway,Description,DHCPEnabled,DHCPServer,IPAddress,IPSubnet,MACAddress
 
-echo on --- Anti-Virus ---
+echo ::::Anti-Virus::::
 wmic /namespace:\\root\securitycenter2 path antivirusproduct
 
-echo on --- Directory Listing and File Search ---
+echo ::::Directory Listing and File Search::::
 REM wmic DATAFILE where "path='\\Users\\[VICTIM_USERNAME]'" GET Name,readable,size
 REM wmic DATAFILE where "drive='C:' AND Name like '%password%'" GET Name,readable,size /VALUE
 wmic DATAFILE where "name like '%.doc'" get Name 
@@ -33,34 +33,34 @@ REM wmic DATAFILE where "name like '%.xlsx'" get Name
 REM wmic DATAFILE where "name like '%.docx'" get Name
 REM wmic DATAFILE where "name like '%.ppt'" get Name
 
-echo on Domain Enumeration:
-echo on --- Domain and DC Info ---
+echo ::::Domain Enumeration::::
+echo ::::Domain and DC Info::::
 wmic NTDOMAIN GET DomainControllerAddress,DomainName,Roles /VALUE
 
-echo on --- Domain User Info ---
+echo ::::Domain User Info::::
 wmic /NAMESPACE:\\root\directory\ldap PATH ds_user where "ds_samaccountname='Admin'"
 
-echo on --- List All Users ---
+echo ::::List All Users::::
 wmic /NAMESPACE:\\root\directory\ldap PATH ds_user GET ds_samaccountname
 
-echo on --- List All Groups ---
+echo ::::List All Groups::::
 wmic /NAMESPACE:\\root\directory\ldap PATH ds_group GET ds_samaccountname
 
-echo on --- Members of A Group ---
+echo ::::Members of A Group::::
 wmic /NAMESPACE:\\root\directory\ldap PATH ds_group where "ds_samaccountname='Domain Admins'" Get ds_member /Value
 
-echo on --- List All Computers ---
+echo ::::List All Computers::::
 wmic /NAMESPACE:\\root\directory\ldap PATH ds_computer GET ds_samaccountname
 wmic /NAMESPACE:\\root\directory\ldap PATH ds_computer GET ds_dnshostname
 
-echo on Misc:
-echo on --- Execute Remote Command ---
+echo ::Misc:::
+echo ::Execute Remote Command::::
 wmic process call create "cmd.exe /c ping google.com"
 
-echo on --- Peripherals ---
+echo ::::Peripherals::::
 wmic path Win32_PnPdevice 
 
-echo on --- Installed Updates ---
+echo ::::Installed Updates::::
 wmic qfe list brief
 
 REM --- Enable Remote Desktop ---
