0402664083a018b25a537003b28c21d27de063af
Bank_Security <m.molte@hotmail.it>
2019-10-10 14:45:34 +0200

    Create Host_Recon.bat

---
+++ Discovery/Host_Recon.bat
@@ -0,0 +1,28 @@
+whoami
+systeminfo
+qwinsta
+quser
+
+net user Administrator /domain
+net Accounts
+net localgroup administrators
+net use
+net share
+net group "domain admins" /domain
+net config workstation
+net accounts
+net accounts /domain
+net view
+
+wmic useraccount get /ALL
+wmic startup list brief
+wmic share list
+wmic service get name,displayname,pathname
+wmic process list brief
+wmic process get caption,executablepath
+wmic qfe get description,installedOn /format:csv
+
+arp -a
+ipconfig /displaydns
+route print
+netsh advfirewall show allprofiles
